833a1ce5a8947e649978ad300802697d84dbed4c
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2023-10-19 09:47:33 +0800

    image labler setup

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -7,9 +7,9 @@ const String baseUrl = isProd ? "" : "https://gatherer.d.helixworlds.io";
 
 const String shopIcon = "assets/shop.png";
 const String helpIcon = "assets/help.png";
-const String cameraIcon = "lib/assets/camera.png";
-const String historyIcon = "lib/assets/history.png";
-const String particleBackground = "lib/assets/particles.png";
+const String cameraIcon = "assets/camera.png";
+const String historyIcon = "assets/history.png";
+const String particleBackground = "assets/particles.png";
 
 setupAssets() async {
 }
--- lib/core/service_di.dart
+++ lib/core/service_di.dart
@@ -1,13 +1,16 @@
 import 'package:app_common_modules/di/services_di.dart';
+import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:helixworlds_snatcher_sdk/features/log/data/log_local_datasource.dart';
 import 'package:helixworlds_snatcher_sdk/features/user_details/user_details_repository.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:helixworlds_snatcher_sdk/features/scan/data/scan_repository.dart';
 import 'package:helixworlds_snatcher_sdk/features/user_details/data/user_details_local_datsource.dart';
 import 'package:dio/dio.dart';
 import 'package:helixworlds_snatcher_sdk/features/user_details/data/user_details_remote_datasource.dart';
 import 'package:helixworlds_snatcher_sdk/utils/image_detector.dart';
-
+import 'dart:io' as io;
 import 'const.dart';
 
 SharedPreferences? _sharedPref;
@@ -31,9 +34,23 @@ Dio _getDio(){
   return getNetworkUtil().getDio(isDebug: true);
 }
 
-_setupMLServices(){
+_setupMLServices() async {
   serviceLocator.registerLazySingleton(() => ImageDetector());
+  const path = 'assets/model.tflite';
+  final modelPath = await _getApplicationPath(path);
+  await io.Directory(dirname(path)).create(recursive: true);
+  final options = LocalLabelerOptions(modelPath: modelPath);
+  serviceLocator.registerLazySingleton(() => ImageLabeler(options: options));
 }
+ImageLabeler getImageLabler(){
+  return serviceLocator<ImageLabeler>();
+}
+
+Future<String> _getApplicationPath(String path) async {
+  var result = await getApplicationSupportDirectory();
+  return "${result.path}/$path";
+}
+
 ImageDetector getImageDetector(){
   return serviceLocator<ImageDetector>();
 }
@@ -67,6 +84,8 @@ _setupLogService(){
 LogLocalDatasource getLogLocalDS(){
   return serviceLocator<LogLocalDatasource>();
 }
+
+
 
 
 
