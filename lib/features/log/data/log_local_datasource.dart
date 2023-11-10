3384d33419ee0e7b8e95ccb428b80d5d55558888
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2023-11-10 19:22:58 +0800

    log local updates

--- lib/features/log/data/log_local_datasource.dart
+++ lib/features/log/data/log_local_datasource.dart
@@ -10,8 +10,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'model/log_model.dart';
 
 const String local_key_logs = "local_key_logs";
+const String local_saved_items_logs = "local_saved_items_logs";
 
 abstract class ILogLocalDatasource {
+  Future<Either<Failure, Success>> cacheSaveItems(List<MyLogModel> logs);
+  Future<Either<Failure, List<MyLogModel>>> getSavedItems();
+
   Future<Either<Failure, Success>> cacheLogs(List<MyLogModel> logs);
   Future<Either<Failure, List<MyLogModel>>> getLogs();
 
@@ -36,6 +40,29 @@ class LogLocalDatasource extends ILogLocalDatasource {
   Future<Either<Failure, List<MyLogModel>>> getLogs() async {
     try{
       final String jsonString =  _sharedPref.getString(local_key_logs) ?? "";
+      final List<dynamic> jsonList = jsonDecode(jsonString);
+      final List<MyLogModel> objects = jsonList.map((json) => MyLogModel.fromJson(json)).toList();
+      return Right(objects);
+    }catch(e){
+      return const Right([]);
+    }
+  }
+  
+  @override
+  Future<Either<Failure, Success>> cacheSaveItems(List<MyLogModel> logs) async {
+    try{
+      final String jsonString = jsonEncode(logs.map((obj) => obj.toJson()).toList());
+      _sharedPref.setString(local_saved_items_logs, jsonString);
+      return Right(CacheSuccess());
+    }catch(e){
+      return Left(LocalDatasourceFailure());
+    }
+  }
+  
+  @override
+  Future<Either<Failure, List<MyLogModel>>> getSavedItems() async {
+    try{
+      final String jsonString =  _sharedPref.getString(local_saved_items_logs) ?? "";
       final List<dynamic> jsonList = jsonDecode(jsonString);
       final List<MyLogModel> objects = jsonList.map((json) => MyLogModel.fromJson(json)).toList();
       return Right(objects);
