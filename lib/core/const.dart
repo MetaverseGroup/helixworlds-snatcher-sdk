d0e08a4a48f6781d0855d553cb2c0394415488d7
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2023-12-08 09:01:52 +0800

    prod env url updated

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -1,5 +1,5 @@
 const bool isProd = true;
-const String baseUrl = isProd ? "https://gatherer.helixworlds.io/" : "https://gatherer.d.helixworlds.io";
+const String baseUrl = isProd ? "https://gatherer.helixworlds.io" : "https://gatherer.d.helixworlds.io";
 const String inventoryUrl = isProd ? "" : "https://localhost:8080/dev/api";
 
 
