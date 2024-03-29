f90b084decd21f582c852719944edcd6d0e60a45
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2024-03-30 07:47:37 +0800

    code clean up

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -7,13 +7,6 @@ String baseUrl = isProd ? "https://gatherer.helixworlds.io" : "https://gatherer.
 // const String baseUrl = isProd ? "http://192.168.254.102:3000" : "https://gatherer.d.helixworlds.io";
 String inventoryUrl = isProd ? "" : "https://localhost:8080/dev/api";
 
-// const String packageName = "helixworlds_snatcher_sdk";
-// const String shopIcon =  "${packageName}lib/assets/shop.png";
-// const String helpIcon = "${packageName}lib/assets/help.png";
-// const String cameraIcon = "${packageName}lib/assets/camera.png";
-// const String historyIcon = "${packageName}lib/assets/history.png";
-// const String particleBackground = "${packageName}lib/assets/particles.png";
-// const String svgPicture = "lib/assets/icon_arrow_right.svg";
 
 
 
