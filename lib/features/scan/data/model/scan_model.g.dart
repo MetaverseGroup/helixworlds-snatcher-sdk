88c871074c7a165efd8a3d68df55fff577477069
Lainel John Dela Cruz <ljbdelacruz123@gmail.com>
2024-08-12 21:05:18 +0800

    v4 scanning object

--- lib/features/scan/data/model/scan_model.g.dart
+++ lib/features/scan/data/model/scan_model.g.dart
@@ -40,6 +40,83 @@ Map<String, dynamic> _$$InventoryItemModelImplToJson(
       'maximumRedemptions': instance.maximumRedemptions,
     };
 
+_$InventoryItemV4ModelImpl _$$InventoryItemV4ModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$InventoryItemV4ModelImpl(
+      id: json['id'] as String?,
+      title: json['title'] as String?,
+      description: json['description'] as String?,
+      storeUrl: json['storeUrl'] as String?,
+      productUrl: json['productUrl'] as String?,
+      images: (json['images'] as List<dynamic>?)
+          ?.map((e) =>
+              InventoryItemV4ImageModel.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$$InventoryItemV4ModelImplToJson(
+        _$InventoryItemV4ModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+      'storeUrl': instance.storeUrl,
+      'productUrl': instance.productUrl,
+      'images': instance.images,
+    };
+
+_$InventoryItemV4ImageModelImpl _$$InventoryItemV4ImageModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$InventoryItemV4ImageModelImpl(
+      file: json['file'] == null
+          ? null
+          : FileInfo.fromJson(json['file'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$InventoryItemV4ImageModelImplToJson(
+        _$InventoryItemV4ImageModelImpl instance) =>
+    <String, dynamic>{
+      'file': instance.file,
+    };
+
+_$ScanVirtualItemModelImpl _$$ScanVirtualItemModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$ScanVirtualItemModelImpl(
+      id: json['id'] as String?,
+      title: json['title'] as String?,
+      description: json['description'] as String?,
+    );
+
+Map<String, dynamic> _$$ScanVirtualItemModelImplToJson(
+        _$ScanVirtualItemModelImpl instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'title': instance.title,
+      'description': instance.description,
+    };
+
+_$ScanResponseModelImpl _$$ScanResponseModelImplFromJson(
+        Map<String, dynamic> json) =>
+    _$ScanResponseModelImpl(
+      virtualItem: json['virtualItem'] == null
+          ? null
+          : ScanVirtualItemModel.fromJson(
+              json['virtualItem'] as Map<String, dynamic>),
+      inventory: json['inventory'] == null
+          ? null
+          : InventoryItemV4Model.fromJson(
+              json['inventory'] as Map<String, dynamic>),
+      code: json['code'] as String?,
+    );
+
+Map<String, dynamic> _$$ScanResponseModelImplToJson(
+        _$ScanResponseModelImpl instance) =>
+    <String, dynamic>{
+      'virtualItem': instance.virtualItem,
+      'inventory': instance.inventory,
+      'code': instance.code,
+    };
+
 _$ImageInfoImpl _$$ImageInfoImplFromJson(Map<String, dynamic> json) =>
     _$ImageInfoImpl(
       file: FileInfo.fromJson(json['file'] as Map<String, dynamic>),
